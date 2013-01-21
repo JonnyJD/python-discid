1e99a7b3244c57e2ab7baccc2f60d92805c7457b
Johannes Dewender <github@JonnyJD.net>
2013-01-21 01:45:23 +0100

    use unittest for tests
    Unittest is a standard Python module.
    
    Other options would be to use py.test and nose
    to discover tests automatically (by naming them test_),
    but these require further packages for testing.
    
    Unittest also supports testdiscovery, but only stating with Python 2.7.

--- test_discid.py
+++ test_discid.py
@@ -3,20 +3,33 @@
 # This test is free. You can redistribute and/or modify it at will.
 
 import sys
+import unittest
+
 import discid
 
-device = discid.DEFAULT_DEVICE
-print("default device: %s" % device)
-if device is None or len(device) == 0:
-    sys.exit("error: device not set")
 
-disc = discid.DiscId()
-if disc is None:
-    sys.exit("error: disc object not created")
+class TestModule(unittest.TestCase):
 
-disc.free()
+    def test_default_device(self):
+        device = discid.DEFAULT_DEVICE
+        self.assertTrue(device, "No default device given")
 
-print("All tests successfull")
+
+class TestClass(unittest.TestCase):
+
+    def setUp(self):
+        self.disc = discid.DiscId()
+        self.assertTrue(self.disc, "No DiscId object created")
+
+    def runTest(self):
+        pass
+
+    def tearDown(self):
+        self.disc.free()
+
+
+if __name__ == "__main__":
+    unittest.main()
 
 
 # vim:set shiftwidth=4 smarttab expandtab:
