03b2761656a885e2aeab46721161dafb36906dcc
Johannes Dewender <github@JonnyJD.net>
2013-01-29 11:55:50 +0100

    add encoding tests
    We also test the internal _encode/_decode functions.
    These are not part of the API, but it might be good
    to test these explicitely (Python 2/3 pitfall).
    We have to make sure that this is in sync with the implementation
    though.

--- test_discid.py
+++ test_discid.py
@@ -8,6 +8,24 @@ import unittest
 import discid
 
 
+class TestModulePrivate(unittest.TestCase):
+
+    # lots of encoding tests, because that is quite different in Python 2/3
+    def test_encode(self):
+        self.assertTrue(type(discid._encode("test")) is type(b"test"))
+        self.assertEqual(discid._encode("test"), b"test")
+
+    def test_decode(self):
+        self.assertTrue(type(discid._decode(b"test")) is type(u"test"))
+        self.assertEqual(discid._decode(b"test"), "test")
+
+    def test_encoding(self):
+        string = "test"
+        self.assertEqual(discid._decode(discid._encode(string)), string)
+        bytestring = b"test"
+        self.assertEqual(discid._encode(discid._decode(bytestring)), bytestring)
+
+
 class TestModule(unittest.TestCase):
 
     def test_default_device(self):
@@ -24,6 +42,12 @@ class TestClass(unittest.TestCase):
     def test_invalid_device(self):
         device = "non_existing_device"
         self.assertRaises(discid.DiscError, self.disc.read, device)
+
+    def test_device_encoding(self):
+        device = u"non_existing_device"
+        self.assertRaises(discid.DiscError, self.disc.read, device)
+        devicebytes = b"non_existing_device"
+        self.assertRaises(discid.DiscError, self.disc.read, devicebytes)
 
     def test_empty_id(self):
         self.assertTrue(self.disc.id is None)
