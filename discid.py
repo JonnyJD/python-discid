2b807d73b3a0f9c04eb08475fcbb3a9514aa0bc7
Johannes Dewender <github@JonnyJD.net>
2013-04-09 19:46:21 +0200

    set version to 0.4.0

--- discid.py
+++ discid.py
@@ -34,7 +34,7 @@ from ctypes import c_int, c_void_p, c_char_p
 from ctypes.util import find_library
 
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 
 _LIB_BASE_NAME = "discid"
 _LIB_MAJOR_VERSION = 0
